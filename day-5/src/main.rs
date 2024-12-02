3844f7f901f718a67513779fcf8211abf7309c7b
Jay Ellis <jgellis206+git@gmail.com>
2024-12-02 11:50:15 -0700

    updated

--- day-5/src/main.rs
+++ day-5/src/main.rs
@@ -1,53 +1,50 @@
 fn main() {
-    println!("{:?}", get_seeds(&get_input().unwrap()));
+    // println!("{:?}", parse_text(&get_input().unwrap()));
+    parse_text(&get_input().unwrap())
 }
 
-fn get_seeds(input: &str) -> Vec<String> {
-    input
-        .lines()
+fn parse_text(input: &str) {
+    let mut data = input.split("\n\n");
+    let seeds: Vec<u32> = data
         .next()
         .unwrap()
         .split_whitespace()
         .filter_map(|s| {
             if s.chars().all(|c| c.is_numeric()) {
-                Some(s.to_string())
+                Some(s.parse::<u32>().unwrap())
             } else {
                 None
             }
         })
-        .collect()
+        .collect();
+    println!("{:?}", seeds);
+
+    let mut maps = Vec::new();
+    for m in data {
+        maps.push(parse_map(m));
+    }
+
+
+}
+
+fn parse_map(input: &str) -> Vec<Vec<u32>> {
+    input
+        .lines()
+        .skip(1)
+        .map(|line| {
+            line.split_whitespace()
+                .filter_map(|s| {
+                    if s.chars().all(|c| c.is_numeric()) {
+                        Some(s.parse::<u32>().unwrap())
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<u32>>()
+        })
+        .collect::<Vec<Vec<u32>>>()
 }
 
 fn get_input() -> Result<String, std::io::Error> {
-    std::fs::read_to_string("/input-test1.txt")
+    std::fs::read_to_string("./input-test1.txt")
 }
-
-// #[tokio::main]
-// async fn get_input() -> Result<String, reqwest::Error> {
-//     // URL of the API endpoint
-//     let url = "https://adventofcode.com/2023/day/5/input";
-//     let client = reqwest::Client::new();
-//     let cookies = std::fs::read_to_string("../cookies.txt").unwrap();
-//
-//     // Make a GET request
-//     let response = client
-//         .get(url)
-//         .header("Accept", "text/html")
-//         .header("Accept-Language", "en-US,en;q=0.9")
-//         .header("Cookie", cookies.trim().to_string())
-//         .header("Cache-Control", "no-cache")
-//         .header("Pragma", "no-cache")
-//         .header("Referer", "https://adventofcode.com/2023/day/5")
-//         .send()
-//         .await?;
-//
-//     // Check if the request was successful
-//     if response.status().is_success() {
-//         // Read the response as text
-//         let text = response.text().await?;
-//
-//         Ok(text)
-//     } else {
-//         Err(response.error_for_status().unwrap_err())
-//     }
-// }
